016fde5625c71941f3891bb14cd7bc3c1251be6b
GrinevichEvgen <grinevichevgen@gmail.com>
2023-01-09 02:14:48 +0300

    Lesson_06

--- Lesson_06/classwork_03.py
+++ Lesson_06/classwork_03.py
@@ -3,5 +3,5 @@ import random
 
 nominal =['6','7','8','9','10','J','D','K','A']
 masti =['Hearts', 'Diamonds', 'Clubs', 'Spades']
-print (random.choice(nominal) , random.choice(masti))
+print (random.choice(nominal) ,  random.choice(masti))
 
