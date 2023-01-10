2318306229ba41cf0fddc59792674fed6716bebe
GrinevichEvgen <grinevichevgen@gmail.com>
2023-01-10 20:21:34 +0300

    Lesson_06

--- Lesson_06/classwork_01.py
+++ Lesson_06/classwork_01.py
@@ -1,23 +1,29 @@
-dictionary = {
-    "appie": "яблоко"
-    "car": "машина"
-}
+import csv
+
+with open("dictionary.csv", "r") as file:
+    reader = csv.reader(file)
+    dictionary = {
+        row[0]: row[1]
+        for row in reader
+    }
 
 
 def eng_to_rus(word):
-    return dictionary.get(word,"ERROR")
+    return dictionary.get(word, "ERROR")
 
-def rus_to_eng(word, "ERROR"):
-    return = {
-        value:key
+
+def rus_to_eng(asd):
+    return {
+        value: key
         for key, value in dictionary.items()
-    }.get(word, "ERROR")
+    }.get(asd, "ERROR")
+
 
 def rus_to_eng_2(word):
     for eng, rus in dictionary.items():
+        if rus == word:
+            return eng
 
 
-
-    return
 print(eng_to_rus("apple"))
-print(rus_to_eng("машина"))
+print(rus_to_eng("зеленый"))
