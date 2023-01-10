ae563605906a73928ff1e8c86da61eb6d85cd598
GrinevichEvgen <grinevichevgen@gmail.com>
2023-01-10 20:19:58 +0300

    Lesson_07

--- Lesson_07/classwork_01.py
+++ Lesson_07/classwork_01.py
@@ -4,11 +4,11 @@
  первое число в паре - координата по горизонтали, второе - координата по вертикали.
  Если ферзи не бьют друг друга, выведите слово NO, иначе выведите YES.'''
 
-first = [3,7]
-last = [4,6]
+first = [3,6]
+last = [4,5]
 
 if __name__ =="__main__":
-    if first[0]==last[0] or first[1]==last[1]:
+    if first[0] == last[0] or first[1] == last[1]:
         print("YES")
     if abs(first[0] - last[0]) == abs(first[1] - last[1]):
         print("NO")
